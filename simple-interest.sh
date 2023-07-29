93bf486d73db5cca9b521b178b592e6c2f163056
faizrazadec <123550860+faizrazadec@users.noreply.github.com>
2023-07-29 18:03:45 +0500

    simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
    # Do not use this in production. Sample purpose only.
    # Author: faiz raza
    # Additional Authors:
-   # <faiz.raza.dec>
+   # faizrazadec
    # Input:
    # p, principal amount
    # t, time period in years
